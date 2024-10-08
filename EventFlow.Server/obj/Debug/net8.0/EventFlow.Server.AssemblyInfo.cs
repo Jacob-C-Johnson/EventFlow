9cc30184d1df111885908d082463cdd8ac43cf2d
Jacob-C-Johnson <jacobj1789@gmail.com>
2024-10-08 10:07:21 -0400

    Application can be run with dotnet run now

--- EventFlow.Server/obj/Debug/net8.0/EventFlow.Server.AssemblyInfo.cs
+++ EventFlow.Server/obj/Debug/net8.0/EventFlow.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventFlow.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+027eb13c148abad9fb0f3b0e17fe91233d81fb8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9625838862a1bc2f097f34dd86eb7ade01d6f01")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventFlow.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventFlow.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
