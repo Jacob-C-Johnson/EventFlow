752808a25f0e63dd0dcb735c0f25f4f0ed32a211
Jacob Johnson <jacobj1789@gmail.com>
2024-11-03 10:31:41 -0500

    database fixed for good

--- Repository/IEventFlowRepository.cs
+++ Repository/IEventFlowRepository.cs
@@ -3,7 +3,7 @@ using EventFlow.Models;
 namespace EventFlow.Repository {
     public interface IEventFlowRepository {
         public Task<int> GetUserId(string userEmail);
-        public Task<DateTime> GetEventReservation(int userId);
+        public Task<List<string>> GetEventReservation(int userId);
         public Task<List<(int EventId, string EventLocation, string EventDescription, int TotalAttendees, string Title)>> GetAllEvents();
         public Task<int> AddUser(string userName, string userEmail);
         public Task<int> AddReservation(string reservationTime, string reservationDate, string status, int userId, int eventId);
