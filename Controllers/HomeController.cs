1fac4c614183e729908498fceda2f6c4437cd007
Jacob Johnson <jacobj1789@gmail.com>
2024-11-03 10:46:43 -0500

    Added GetReservation Controller action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,33 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using EventFlow.Models;
+using EventFlow.Repository;
 
 namespace EventFlow.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IEventFlowRepository _eventFlowRepository;
 
-    public HomeController(ILogger<HomeController> logger)
+
+    public HomeController(ILogger<HomeController> logger, IEventFlowRepository eventFlowRepository)
     {
         _logger = logger;
+        _eventFlowRepository = eventFlowRepository;
     }
 
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpPost]
+    [Route("/GetReservation")]
+    public async Task<IActionResult> GetReservation(int reservationid)
+    {
+        var reservation = await _eventFlowRepository.GetReservation(reservationid);   
+        return Json(new { reservation });
     }
 
     public IActionResult Privacy()
