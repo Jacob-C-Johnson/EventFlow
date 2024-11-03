312e827bcd65b6787c01c18d4fc28508c5488974
DevinDGuo <143441155+DevinDGuo@users.noreply.github.com>
2024-11-03 09:51:55 -0500

    Update IEventFlowRepository.cs

--- EventFlow.Server/Repository/IEventFlowRepository.cs
+++ EventFlow.Server/Repository/IEventFlowRepository.cs
@@ -4,5 +4,10 @@ namespace EventFlow.Repository {
     public interface IEventFlowRepository {
         public Task<int> GetUserId(string userEmail);
         public Task<DateTime> GetEventReservation(int userId);
-    }
+        public Task<List<(int EventId, string EventLocation, string EventDescription, int TotalAttendees, string Title)>> GetAllEvents();
+        public Task<int> AddUser(string userName, string userEmail);
+        public Task<int> AddReservation(string reservationTime, string reservationDate, string status, int userId, int eventId);
+        public Task DeleteReservation(int reservationId);
+        public Task<List<(int ReservationId, string ReservationTime, string ReservationDate, string Status, int UserId, int EventId)>> GetReservation(int reservationId);
+            }
 }
