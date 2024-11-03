2b062a11e7d85679ad22d30a3da1287a9d094fd9
DevinDGuo <143441155+DevinDGuo@users.noreply.github.com>
2024-11-03 09:47:41 -0500

    Update EventFlowRepository.cs

--- EventFlow.Server/Repository/EventFlowRepository.cs
+++ EventFlow.Server/Repository/EventFlowRepository.cs
@@ -22,18 +22,21 @@ namespace EventFlow.Repository {
             } return userId;
         }
 
-        public async Task<DateTime> GetEventReservation(int userId) {
+        public async Task<List<string>> GetEventReservation(int userId) {
             using (MySqlConnection _connection = new MySqlConnection(_connectionString)) {
+                List<string> reservationDetails = new List<string>();
                 await _connection.OpenAsync();
                 var statement1 = "SELECT reservation_time, reservation_date FROM reservation WHERE User_user_id = @userEmail";
                 using (MySqlCommand command = new MySqlCommand(statement1, _connection)) {
                     command.Parameters.Add(new MySqlParameter("@userEmail", MySqlDbType.VarChar, 100) {Value = userId});
                     using (MySqlDataReader reader = (MySqlDataReader)await command.ExecuteReaderAsync()) {
                         if (await reader.ReadAsync()) {
-                            var reservationTime = reader.GetTimeSpan(reader.GetOrdinal("reservation_time"));
-                            var reservationDate = reader.GetDateTime(reader.GetOrdinal("reservation_date"));
+                            var reservationTime = reader.GetString(reader.GetOrdinal("reservation_time"));
+                            var reservationDate = reader.GetString(reader.GetOrdinal("reservation_date"));
+                            reservationDetails.append(reservatoinTime);
+                            reservationDetails.append(reservationDate);
 
-                            return reservationDate.Date + reservationTime;
+                            return reservationDetails;
                         } else {
                             throw new Exception("No reservation found for this user.");
                         }
